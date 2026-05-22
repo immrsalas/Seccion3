53e3555c89910d8677e1c87c759aa7fe5873950c
MrSalas <mrsalasb@outlook.com>
2026-05-21 23:49:01 -0600

    Programa para saber si un numero es par o impar - Condicionales

--- Program.cs
+++ Program.cs
@@ -9,58 +9,17 @@ namespace Seccion3
     {
         static void Main(string[] args)
         {
-            // Hacer un programa que le pida al usuario un número del 1 al 12 y escriba el nombre del mes que corresponde ese número en el calendario.Agregar un case default.
+            //Hacer un programa que le pida al usuario un número y decirle si éste es par o impar.
 
-            Console.WriteLine("Programa para saber el mes del año ingresando un numero del 1 al 12");
-            Console.WriteLine("- - - - - - - - - - - - - - - - - - - -");
+            Console.WriteLine("Programa para saber si un numero es par o impar");
 
-            Console.WriteLine("Ingrese un numero del 1 al 12");
-            byte numDelAño = byte.Parse(Console.ReadLine());
+            Console.Write("Ingresa un numero: ");
+            int numero = int.Parse(Console.ReadLine());
 
-            switch (numDelAño)
-            {
-                case 1:
-                    Console.Write("Enero");
-                    break;
-                case 2:
-                    Console.Write("Febrero");
-                    break;
-                case 3:
-                    Console.Write("Marzo");
-                    break;
-                case 4:
-                    Console.Write("Abril");
-                    break;
-                case 5:
-                    Console.Write("Mayo");
-                    break;
-                case 6:
-                    Console.Write("Junio");
-                    break;
-                case 7:
-                    Console.Write("Julio");
-                    break;
-                case 8:
-                    Console.Write("Agosto");
-                    break;
-                case 9:
-                    Console.Write("Septiembre");
-                    break;
-                case 10:
-                    Console.Write("Octubre");
-                    break;
-                case 11:
-                    Console.Write("Noviembre");
-                    break;
-                case 12:
-                    Console.Write("Diciembre");
-                    break;
-                default:
-                    Console.WriteLine("Solo puedes ingresar del 1 al 12");
-                    break;
-            }
-            
-            
+            if (numero % 2 == 0) { Console.WriteLine("Tu numero {0} es par", numero); }
+
+            else { Console.WriteLine("Tu numero {0} es impar", numero); }
+           
             Console.ReadKey();
         }
  
